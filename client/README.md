2e9dc945245a0bc250845c52d0c89ffefed4d25b
Jeff Marston <jmarston@gmail.com>
2017-06-26 18:03:29 -0400

    refactored to api and model layers

--- client/README.md
+++ client/README.md
@@ -16,8 +16,9 @@ TODO
 ==============
 x `Create all actions from metadata, even Ack, Fill, Reject`
     x `Simple variable replacement` 
-    - `Random id() function`
-- `Persist action templates via REST API, redis`
+    x `Random id() function`
+x `Persist action templates via REST API, redis`
+- `Improve seeding to not require 2 refreshes`
 - `Parse Fix XML in Node and expose via REST API`
 - `Get sessions from redis, update status when they change`
 - `Angular router, url matching session`
--- server/models/template-model.js
+++ server/models/template-model.js
@@ -3,25 +3,61 @@
 let _ = require('lodash');
 let bluebird = require('bluebird');
 let redis = require('redis');
-let client = redis.createClient();
+let redisClient = redis.createClient();
 bluebird.promisifyAll(redis.RedisClient.prototype);
 
 class TemplateModel {
-    
+
     static getAll() {
-        return client.lrangeAsync('sessions', 0, 300).then((items) => {
-            return _.map(items, (o)=> {return JSON.parse(o)} );
+        return redisClient.lrangeAsync("templates", 0, 300).then((labels) => {
+            if (labels.length === 0) {
+                return [];
+            }
+            let keys = _.map(labels, o => "template:" + o);
+            return redisClient.mgetAsync(keys).then(o => {
+                return _.map(o, str => JSON.parse(str));
+            });
         });
     }
 
-    static create(sessionName) {
-        let newSession = { name: sessionName };
-        return client.rpushAsync('sessions', JSON.stringify(newSession) );
+    static seedInitial(seedActions) {
+        let promises = [];
+
+        seedActions.forEach(function (template) {
+            let templateJson = JSON.stringify(template);
+            promises.push(redisClient.rpushAsync("templates", template.label).then(o => {
+                return redisClient.msetAsync("template:" + template.label, templateJson).then(p => {
+                    console.log("{ status: '" + p + "' }");
+                });
+            }));
+        }, this);
+        return Promise.all(promises).then(o => {
+            return seedActions;
+        });
     }
 
-    static delete(sessionName) {
-        let newSession = { name: sessionName };
-        return client.lremAsync('sessions', 1, JSON.stringify(newSession) );
+    static create(label, template) {
+        
+        let templateJson = JSON.stringify(template);
+        return redisClient.lrangeAsync("templates", 0, 300).then((labels) => {
+            if (labels.indexOf(label) > -1) {
+                // update
+                console.log("Update existing template: " + label);
+                return redisClient.msetAsync("template:" + label, templateJson);
+            } else {
+                // create
+                console.log("Create new template: " + label);
+                return redisClient.rpushAsync("templates", label).then(o => {
+                    return redisClient.msetAsync("template:" + label, templateJson);
+                });
+            }
+        });
+    }
+
+    static delete(label) {
+        return redisClient.delAsync("template:" + label).then(n => {
+            return redisClient.lremAsync("templates", 100, label);
+        });
     }
 }
 
