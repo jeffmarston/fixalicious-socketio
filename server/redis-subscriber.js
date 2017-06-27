1f6b52d76831f2681d5aaa94535a6d371e070c7d
Marston <jmarston@ezesoft.com>
2017-06-27 16:06:41 -0400

    can't block on smae redis client

--- server/redis-subscriber.js
+++ server/redis-subscriber.js
@@ -7,35 +7,44 @@ let argv = require('yargs').argv;
 bluebird.promisifyAll(redis.RedisClient.prototype);
 
 
-var redisSub = redis.createClient();
-var redisClient = redis.createClient();
+var redisSessions = redis.createClient();
+var redisTransactions = redis.createClient();
 var msg_count = 0;
 
 class Subscriber {
 
     static subscribeAll() {
-        
+
         let suffix = (argv.subscriber) ? "-" + argv.subscriber : "-ui";
-        console.log("subscribing to redis channels with suffix: " + sub)
+        let sub_sessionKey = "fix-svr-sessions" + suffix;
+        let sub_transactionKey = "fix-svr-BAXA" + suffix;
+        let my_sessionKey = "ui-sessions";
+        let my_transactionKey = "ui-transactions-BAXA";
 
         function pollForSessions(err, result) {
             let session = JSON.parse(result[1]);
             console.log("BRPOPed session: " + result[1]);
             global.io.emit('session', session);
-            
-            redisClient.hsetAsync('ui-sessions', session.session, result[1]).then(o => {
-                return redisClient.brpop("fix-svr-sessions" + sub, 0, pollForSessions);
+
+            redisSessions.hsetAsync(my_sessionKey, session.session, result[1]).then(o => {
+                setTimeout(() => {
+                    return redisSessions.brpop(sub_sessionKey, 0, pollForSessions)
+                }, 40);
             });
         }
-        redisClient.brpop("fix-svr-sessions" + sub, 0, pollForSessions);
+        console.log("Watching on Redis key: " + sub_sessionKey);
+        redisSessions.brpop(sub_sessionKey, 0, pollForSessions);
 
 
         function pollForTransactions(err, transaction) {
             //console.log("BRPOPed transaction: " + transaction);
             global.io.emit("transaction", transaction);
-            redisClient.brpoplpush("fix-svr-BAXA" + sub, "ui-transactions-BAXA", 0, pollForTransactions);
+            setTimeout(() => {
+                redisTransactions.brpoplpush(sub_transactionKey, my_transactionKey, 0, pollForTransactions)
+            }, 40);
         }
-        redisClient.brpoplpush("fix-svr-BAXA" + sub, "ui-transactions-BAXA", 0, pollForTransactions);
+        console.log("Watching on Redis key: " + sub_transactionKey);
+        redisTransactions.brpoplpush(sub_transactionKey, my_transactionKey, 0, pollForTransactions);
 
     }
 
