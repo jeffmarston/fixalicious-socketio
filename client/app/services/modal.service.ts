6e8499213f00655c52dfdee76fc8648a3c6ead05
Jeff Marston <jmarston@gmail.com>
2017-07-23 20:16:11 -0400

    combined modal into one file

--- client/app/services/modal.service.ts
+++ client/app/services/modal.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import * as _ from "lodash";
+import { Component, Injectable, ElementRef, EventEmitter } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Http, Response } from "@angular/http";
 
 @Injectable()
 export class ModalService {
@@ -8,4 +9,97 @@ export class ModalService {
     public show(displayText) {
         this.onShow.emit(displayText);
     };
+}
+
+@Component({
+    selector: 'modal-placeholder',
+    template: `
+        <div class="modal-form" tabindex="0"
+            (keyup.esc)="close()"
+            [style.opacity]="formOpacity">
+            <header>
+                <h1>FIX Message Detail</h1>
+            </header>
+            <section>
+                <textarea readonly [ngModel]="displayText"></textarea>
+            </section>
+            <footer>
+                <button (click)="close()">Close</button>
+            </footer>
+        </div>
+    `,
+    styles: [` 
+    :host {
+        position: fixed;
+        width: 100%;
+        height: 0;
+        top: 0;
+        left: 0;
+        background: rgba(0, 0, 0, 0.5);
+        z-index: 200;
+        overflow: hidden;
+    }
+
+    div.modal-form {
+        transition: opacity .2s linear;
+        width: 70%;
+        height: 50%;
+        background: #fff;
+        margin: 40px auto auto auto;
+        display: flex;
+        flex-direction: column;
+    }
+
+    section {
+        flex: 1 1 auto;
+        display: flex;
+    }
+
+    textarea {
+        flex: 1 1 auto;
+        border: none;
+        outline: none;
+        resize: none;
+        padding: 6px;
+    }
+
+    header {
+        padding: 6px;
+        justify-content: center;
+        display: flex;
+        border-bottom: 1px solid gray;
+    }
+    footer {
+        padding: 6px;
+        justify-content: flex-end;
+        display: flex;
+    }
+    footer button {
+        padding: 4px;
+        width: 70px;
+    }
+    `],
+    providers: []
+})
+export class ModalComponent {
+    private hostStyle;
+    private hostElement;
+    private formOpacity: number;
+    private displayText: string;
+
+    constructor($element: ElementRef,
+        modalService: ModalService) {
+        this.hostStyle = $element.nativeElement.style;
+
+        modalService.onShow.subscribe(o => {
+            this.hostStyle.height = "100%";
+            this.formOpacity = 1;
+            this.displayText = o;
+        });
+    }
+
+    private close() {
+        this.hostStyle.height = "0";
+        this.formOpacity = 0;
+    }
 }
--- readme.md
+++ readme.md
@@ -2,10 +2,11 @@ FIXalicious UI
 ==============
 
 FIXalicious UI is a front end for Rob Baxter's excellent FIXalicious back end.
-It uses Angular2, ag-grid, redis, and socket.io.
+It uses Angular, ag-grid, redis, and socket.io.
 
 ## Prerequisites
 - node.js [https://nodejs.org](https://nodejs.org)
+- typescript [https://www.typescriptlang.org](https://www.typescriptlang.org)
 - redis [https://redis.org](https://redis.io)
 - fixalicious [https://github.com/rbaxter1](https://github.com/rbaxter1/fixalicious)
 
@@ -13,7 +14,7 @@ It uses Angular2, ag-grid, redis, and socket.io.
 # Design
 Fixalcious-ui is a front end to Fixalicious, so it is quite useless by itself. This project has two parts, a node.js server and an Angular client.
 
-The server communicates to Fixalicious using shared keys in Redis for realtime messaging, and HTTP REST calls to send new messages. It also persists configuration for the client, such as quick action templates.
+The server communicates to Fixalicious using shared keys in Redis for realtime messaging, and HTTP REST calls to send new messages. It also persists configuration for the client, such as action templates and scenarios.
 
 The client calls the server APIs to get data, and subscribes to a socket.io channel for realtime updates of FIX transations and session information.
 
@@ -35,18 +36,18 @@ The server only needs dependencies installed, so change to the server folder, in
 ```
 > cd server
 > npm install
-> node ./bin/www --subscriber=ui
+> node server.js
 ```
 
-That's it, you're up and running with the defaults. Navigate to localhost:3000 or go directly to the client.
+That's it, you're up and running with the defaults. Navigate to localhost:4400 or go directly to the client.
 ```
-http://localhost:3000
+http://localhost:4400/client
 ```
 
 ## options
 
-Fixalicious can publish on multiple channels. By default, we listen to the channel "ui". This can be changed using the "subscriber" argument.
+Fixalicious can publish on multiple channels. By default, we listen to the channel "ui". This can be changed using the "subscriber" argument. For all options:
 
 ```
-node ./bin/www --subscriber=ui
+node server.js -?
 ```
